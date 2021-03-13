f740c2f19608a53fd1ef3921c88c857c7331a733
Anton Yasnov <maza4aka@icloud.com>
2021-03-13 16:01:00 +0200

    Implement AuthImpl logic
    *except for password hashing (TODO!)

--- src/main/java/edu/kpi/testcourse/auth/AuthImpl.java
+++ src/main/java/edu/kpi/testcourse/auth/AuthImpl.java
@@ -1,17 +1,134 @@
 package edu.kpi.testcourse.auth;
 
+import edu.kpi.testcourse.Main;
+import edu.kpi.testcourse.storage.namespace.Namespace;
+import java.util.Arrays;
+
 /**
  * Authentication service implementation.
  */
 public class AuthImpl implements Auth {
+  private final Namespace authStorage = Main.getAuthStorage();
 
   @Override
   public AuthStatus createUser(String username, String password) {
-    return AuthStatus.InvalidCredentials;
+    AuthStatus status;
+    if ((status = checkCredentials(username, password)) != AuthStatus.Ok) {
+      return status;
+    }
+    if ((status = validateUser(username, password)) != AuthStatus.Ok) {
+      return status;
+    }
+    return status;
   }
 
   @Override
   public AuthStatus loginUser(String username, String password) {
-    return AuthStatus.InvalidCredentials;
+    AuthStatus status;
+    if ((status = checkCredentials(username, password)) != AuthStatus.Ok) {
+      return status;
+    }
+    if ((status = registerUser(username, password)) != AuthStatus.Ok) {
+      return status;
+    }
+    return status;
+  }
+
+  /**
+   * Checks username contents.
+   *
+   * @param username username
+   * @return InvalidUsername or Ok
+   */
+  private AuthStatus checkUsername(String username) {
+    if (username != null && username.isEmpty()) {
+      return AuthStatus.InvalidUsername;
+    }
+    return AuthStatus.Ok;
+  }
+
+  /**
+   * Checks password contents.
+   *
+   * @param password password
+   * @return InvalidPassword or Ok
+   */
+  private AuthStatus checkPassword(String password) {
+    if (password != null && password.isEmpty()) {
+      return AuthStatus.InvalidPassword;
+    }
+    return AuthStatus.Ok;
+  }
+
+  /**
+   * Checks credentials contents.
+   *
+   * @param username username
+   * @param password password
+   * @return InvalidCredentials, InvalidUsername, InvalidPassword or Ok
+   */
+  public AuthStatus checkCredentials(String username, String password) {
+    final AuthStatus usernameStatus = checkUsername(username);
+    final AuthStatus passwordStatus = checkPassword(password);
+
+    if ((usernameStatus != AuthStatus.Ok) && (passwordStatus != AuthStatus.Ok)) {
+      return AuthStatus.InvalidCredentials;
+    }
+    if (usernameStatus != AuthStatus.Ok) {
+      return AuthStatus.InvalidUsername;
+    }
+    if (passwordStatus != AuthStatus.Ok) {
+      return AuthStatus.InvalidPassword;
+    }
+    return AuthStatus.Ok;
+  }
+
+  /**
+   * Check that user exists.
+   *
+   * @param username username
+   * @return true if user exists
+   */
+  public boolean isUserExists(String username) {
+    try {
+      return authStorage.get(username) != null;
+    } catch (NullPointerException e) {
+      return false;
+    }
+  }
+
+  /**
+   * Register user with given name and password.
+   *
+   * @param username username
+   * @param password password
+   * @return AuthStatus value
+   */
+  private AuthStatus registerUser(String username, String password) {
+    if (isUserExists(username)) {
+      return AuthStatus.ExistingUser;
+    }
+    byte[] notYetHashedPassword = password.getBytes();
+    authStorage.set(username, notYetHashedPassword);
+    return AuthStatus.Ok;
+  }
+
+  /**
+   * Check user`s credentials correctness.
+   *
+   * @param username username
+   * @param password password
+   * @return AuthStatus value
+   */
+  private AuthStatus validateUser(String username, String password) {
+    if (!isUserExists(username)) {
+      return AuthStatus.InvalidUsername;
+    }
+    byte[] notYetHashedPassword = password.getBytes();
+    byte[] retrievedNotYetHashedPassword = authStorage.get(username);
+    if (!Arrays.equals(retrievedNotYetHashedPassword, notYetHashedPassword)) {
+      return AuthStatus.InvalidPassword;
+    }
+    return AuthStatus.Ok;
   }
 }
